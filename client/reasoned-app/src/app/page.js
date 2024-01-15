2afbd2608edf2f16517433e5abcf3e8194cbe2b7
Emily Louk <emilyrl222@gmail.com>
2024-01-15 16:39:28 -0500

    Server component test

--- client/reasoned-app/src/app/page.js
+++ client/reasoned-app/src/app/page.js
@@ -1,4 +1,19 @@
-import Image from 'next/image'
+import { sql } from "@vercel/postgres";
+import Image from 'next/image';
+
+export default async function Cart({ params }) {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
