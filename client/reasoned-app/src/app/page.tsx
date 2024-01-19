6aab6251e3ee496811db8dabbe3ee6c5cf1bf677
Emily Louk <emilyrl222@gmail.com>
2024-01-19 18:12:15 -0500

    Add Typescript and convert

---
+++ client/reasoned-app/src/app/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import "../App.css";
+
+const Page = () => {
+  return(
+    <div
+      className={"constainer mx-auto flex items-center border-b-2 px-6 py-2 h-24"}
+    >
+
+    <h1 className="font-bold" >ReasonEd</h1>
+    <div className="grow">
+      <div className="flex items-center justify-center gap-2 md:gap-8">
+        <Link href="students">Students</Link>
+        <Link href="students">Teachers</Link>
+        <Link href="students">About</Link>
+      </div>
+    </div>
+      <div>
+        <Link href="signup" className="mr-2 font-bold">Sign Up</Link>
+        <Link href="login" className="font-bold">Log In</Link>
+      </div>
+    </div>
+    
+  );
+};
+export default Page;
