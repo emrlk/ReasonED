2aeb71cff1f8b41ae9644d5885acab22b1d2de49
Emily Louk <emilyrl222@gmail.com>
2024-02-06 20:36:25 -0500

    Initial Godot Project Commit

---
+++ StrawManny/scripts/player.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+var velocity : Vector2 = Vector2()
+var direction : Vector2 = Vector2()
+var sprite : Sprite
+
+func _ready():
+	sprite = $Sprite
+	Engine.set_target_fps(Engine.get_iterations_per_second())
+	
+func read_input():
+	velocity = Vector2()
+	
+	# Move the player and change its direction according to the keybind
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		direction = Vector2(0,-1)
+
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+		direction = Vector2(0,1)
+
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+		direction = Vector2(-1,0)
+		
+		#flips sprite in the direction of movement
+		#must be same scaling as sprite texture
+		sprite.scale.x = 1 
+		
+
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+		direction = Vector2(1,0)
+		
+		#flips sprite in the direction of movement
+		#must be same scaling as sprite texture
+		sprite.scale.x = -1
+
+
+
+	# Prevent diagonal movement from being twice as fast as up, down, etc. individually
+	velocity = velocity.normalized()
+	velocity = move_and_slide(velocity * 400)
+	
+	
+# Read user input 60 times/second
+func _physics_process(delta):
+	read_input()
+	
