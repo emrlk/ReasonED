1151acc81f9e7fd1da0cb45f73a57c3bfdebd3e1
amorg025 <amorg025@odu.edu>
2024-02-06 20:26:06 -0500

    Comment out page body on home page

--- client/reasoned-app/src/app/page.tsx
+++ client/reasoned-app/src/app/page.tsx
@@ -8,48 +8,49 @@ interface ButtonProps {
   disabled: boolean;
 }
 
-function Button({ title, disabled}: ButtonProps) {
+function Button({ title, disabled }: ButtonProps) {
   return (
     <button disabled={disabled}>{title}</button>
   );
 }
 
 const Page = () => {
-  return(
+  return (
     <>
-        {/**Navbar */}
-    <div className={"constainer bg-orange text-purple text-2xl mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
-      <Image
-        src="/ReasonEDLogo.png"
-        width={250}
-        height={190}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-      <div className="grow">
-        <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white">
-          <Link href="students">Students</Link>
-          <Link href="teachers">Teachers</Link>
-          <Link href="about">About</Link>
+      {/**Navbar */}
+      <div className={"constainer bg-orange text-purple text-2xl mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
+        <Image
+          src="/ReasonEDLogo.png"
+          width={250}
+          height={190}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+        <div className="grow">
+          <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white">
+            <Link href="students">Students</Link>
+            <Link href="teachers">Teachers</Link>
+            <Link href="about">About</Link>
+          </div>
+        </div>
+        <div className={"text-lg "}>
+          <Link href="/sign-up" className="mr-2">Sign Up</Link>
+          <Link href="/log-in" className="font-bold">Log In</Link>
         </div>
       </div>
-      <div className={"text-lg "}>
-        <Link href="/sign-up" className="mr-2">Sign Up</Link>
-        <Link href="/log-in" className="font-bold">Log In</Link>
-      </div>
-    </div>
-    
-    {/**Page Body */}
-    <div className={"constainer bg-orange mx-auto flex justify-center items-center border-b-2 px-6 py-2 h-24"}>
 
-      <div className ="font-bold text-white text-xl">
-        <Button title="Elementary School" disabled={false}/>
+      {/**Page Body */}
+      {/*<div className={"constainer bg-orange mx-auto flex justify-center items-center border-b-2 px-6 py-2 h-24"}>
+
+        <div className="font-bold text-white text-xl">
+          <Button title="Elementary School" disabled={false} />
+        </div>
+
       </div>
-      
-    </div>
+  */}
+
+  
     </>
-
-    
   );
 };
 export default Page;
