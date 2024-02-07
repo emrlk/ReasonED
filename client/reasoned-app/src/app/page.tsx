ccc8789797a3ad9636eee8bcc0f01d6177d88427
Emily Louk <emilyrl222@gmail.com>
2024-02-06 20:27:37 -0500

    Change text size

--- client/reasoned-app/src/app/page.tsx
+++ client/reasoned-app/src/app/page.tsx
@@ -18,7 +18,7 @@ const Page = () => {
   return(
     <>
         {/**Navbar */}
-    <div className={"constainer bg-orange text-purple text-2xl mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
+    <div className={"constainer bg-orange text-purple text-xl mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
       <Image
         src="/ReasonEDLogo.png"
         width={250}
@@ -33,18 +33,19 @@ const Page = () => {
           <Link href="about">About</Link>
         </div>
       </div>
-      <div className={"text-lg "}>
-        <Link href="signup" className="mr-2">Sign Up</Link>
+      <div className={"text-sm"}>
+        <Link href="signup" className="mr-2 font-semibold">Sign Up</Link>
         <Link href="login" className="font-bold">Log In</Link>
       </div>
     </div>
     
     {/**Page Body */}
-    <div className={"constainer bg-orange mx-auto flex justify-center items-center border-b-2 px-6 py-2 h-24"}>
+    <div className={"constainer bg-orange h-screen mx-auto flex justify-center items-center  "}>
 
       <div className ="font-bold text-white text-xl">
         <Button title="Elementary School" disabled={false}/>
       </div>
+
       
     </div>
     </>
