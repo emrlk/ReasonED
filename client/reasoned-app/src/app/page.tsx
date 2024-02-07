47cd43ef4cd3beed26899334145c7d5be968ce0d
amorg025 <amorg025@odu.edu>
2024-02-06 22:19:24 -0500

    Place nav bar in header file

--- client/reasoned-app/src/app/page.tsx
+++ client/reasoned-app/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Header from '@/components/common/header';
 
 interface ButtonProps {
   // Text inside the button
@@ -18,26 +19,7 @@ const Page = () => {
   return (
     <>
       {/**Navbar */}
-      <div className={"constainer bg-orange text-purple text-xl mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
-        <Image
-          src="/ReasonEDLogo.png"
-          width={250}
-          height={190}
-          className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
-        />
-        <div className="grow">
-          <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white">
-            <Link href="students">Students</Link>
-            <Link href="teachers">Teachers</Link>
-            <Link href="about">About</Link>
-          </div>
-        </div>
-        <div className={"text-sm"}>
-          <Link href="/sign-up" className="mr-2 font-semibold">Sign Up</Link>
-          <Link href="/log-in" className="font-bold">Log In</Link>
-        </div>
-      </div>
+      <Header />
 
       {/**Page Body */}
       <div className={"constainer bg-orange h-screen mx-auto flex justify-center items-center"}>
---
+++ client/reasoned-app/src/components/common/header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <div className={"constainer bg-orange text-purple text-xl mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
+            <Image
+                src="/ReasonEDLogo.png"
+                width={250}
+                height={190}
+                className="hidden md:block"
+                alt="ReasonED Logo"
+            />
+            <div className="grow">
+                <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white">
+                    <Link href="students">Students</Link>
+                    <Link href="teachers">Teachers</Link>
+                    <Link href="about">About</Link>
+                </div>
+            </div>
+            <div className={"text-sm"}>
+                <Link href="/sign-up" className="mr-2 font-semibold">Sign Up</Link>
+                <Link href="/log-in" className="font-bold">Log In</Link>
+            </div>
+        </div>
+    );
+}
