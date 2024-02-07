b24eb738e1e7d08ed1b3fcd1d0a81f04858a559c
amorg025 <amorg025@odu.edu>
2024-02-06 21:39:31 -0500

    Add animation effects to buttons

--- client/reasoned-app/src/app/page.tsx
+++ client/reasoned-app/src/app/page.tsx
@@ -44,21 +44,21 @@ const Page = () => {
         <div className="font-bold text-white text-xl">
           {/*<Button title="Elementary School" disabled={false} />*/}
           <div className="w-70 mb-4">
-            <button className="bg-purple text-white w-full px-4 py-2 rounded-md">Elementary School</button>
+            <button className="bg-purple text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">Elementary School</button>
           </div>
           <div className="w-70 mb-4">
-            <button className="bg-red-500 text-white w-full px-4 py-2 rounded-md">Middle School</button>
+            <button className="bg-red-500 text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">Middle School</button>
           </div>
           <div className="w-70 mb-4">
-            <button className="bg-blue-500 text-white w-full px-4 py-2 rounded-md">High School</button>
+            <button className="bg-blue-500 text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">High School</button>
           </div>
           <div className="w-70">
-            <button className="bg-yellow text-white w-full px-4 py-2 rounded-md">College</button>
+            <button className="bg-yellow text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">College</button>
           </div>
         </div>
       </div >
     </>
-    
+
   );
 };
 export default Page;
