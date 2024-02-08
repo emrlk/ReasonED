a71a1e1ee76ebd727755408e19b8f8bc39b6dd38
amorg025 <amorg025@odu.edu>
2024-02-07 21:58:49 -0500

    Change footer colors

--- client/reasoned-app/src/components/common/footer.tsx
+++ client/reasoned-app/src/components/common/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
         <footer className="bg-white text-purple font-bold py-2">
             <div className="container mx-auto text-center">
                 <p>
-                    <a href="https://www.odu.edu" className="text-sky-400 hover:underline">Old Dominion University</a><br />
-                    <a href="https://www.cs.odu.edu" className="text-sky-400 hover:underline">ODU Computer Science Department</a><br />
+                    <a href="https://www.odu.edu" className="text-blue-500 hover:underline">Old Dominion University</a><br />
+                    <a href="https://www.cs.odu.edu" className="text-blue-500 hover:underline">ODU Computer Science Department</a><br />
                     &copy; ReasonED.io. All rights reserved.
                 </p>
             </div>
