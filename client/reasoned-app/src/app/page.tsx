fce9f520a3edbfcd168be403aa0e72873d9a9806
amorg025 <amorg025@odu.edu>
2024-02-08 21:57:21 -0500

    Change spacing in title and buttons

--- client/reasoned-app/src/app/page.tsx
+++ client/reasoned-app/src/app/page.tsx
@@ -25,16 +25,16 @@ const Page = () => {
       {/**Page Body */}
       <div className={"constainer bg-orange h-screen mx-auto flex justify-center items-center flex-col text-center mt-3"}>
         <h1 className="font-bold text-8xl text-purple mb-3 hover:scale-105 transition duration-300">ReasonED<span className="text-white">.io</span></h1>
-        <p className="text-lg text-purple mb-24 hover:scale-105 transition duration-300">Empower Critical Thinking with Engaging Games!</p>
-        <div className="font-bold text-white text-xl">
+        <p className="text-lg text-purple mb-16 hover:scale-105 transition duration-300">Empower Critical Thinking with Engaging Games!</p>
+        <div className="font-bold text-white text-xl flex flex-col gap-1/2">
           {/*<Button title="Elementary School" disabled={false} />*/}
-          <div className="w-60 mb-4">
+          <div className="w-60 mb-5">
             <button className="bg-purple text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">Elementary School</button>
           </div>
-          <div className="w-60 mb-4">
+          <div className="w-60 mb-5">
             <button className="bg-red-500 text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">Middle School</button>
           </div>
-          <div className="w-60 mb-4">
+          <div className="w-60 mb-5">
             <button className="bg-blue-500 text-white w-full px-4 py-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">High School</button>
           </div>
           <div className="w-60">
