e4e61b34fd804f5bb446df83846dca31fcb12b2d
amorg025 <amorg025@odu.edu>
2024-02-16 17:03:11 -0500

    Encrypt passwords before storing them in database

--- server/routes/form-submission.js
+++ server/routes/form-submission.js
@@ -1,8 +1,13 @@
+const bcrypt = require('bcrypt');
+
 const handleFormSubmission = async (req, res, client) => {
     const { email, username, password, dob } = req.body;
     try {
+        // Encrypt the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         // Insert the new user into the users table
-        const result = await client.query('INSERT INTO users (email, username, password, dob) VALUES ($1, $2, $3, $4) RETURNING id', [email, username, password, dob]);
+        const result = await client.query('INSERT INTO users (email, username, password, dob) VALUES ($1, $2, $3, $4) RETURNING id', [email, username, hashedPassword, dob]);
         const newUserId = result.rows[0].id;
 
         console.log('New user created with ID:', newUserId);
