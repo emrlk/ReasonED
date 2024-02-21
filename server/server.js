31ce848635b28d0b88a154ecc8bd96db7367ef32
amorg025 <amorg025@odu.edu>
2024-02-20 21:44:01 -0500

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3001; // prev 5000
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); // Use this link if accessing through vercel -> https://reasoned.vercel.app
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Use this link if accessing through vercel -> https://reasoned.vercel.app
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
