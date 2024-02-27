6acad5aaf69336360c667495d124d1586f4dedd1
June Troyer <jtroy003@odu.edu>
2024-02-27 08:31:24 -0500

    Added main menu

---
+++ StrawManny/scripts/menu_buttons.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+var main_bus = AudioServer.get_bus_index('Master')
+
+
+func _on_Volume_pressed():
+	AudioServer.set_bus_mute(main_bus, not AudioServer.is_bus_mute(main_bus))
+
+
+
+func _on_Tutorial_pressed():
+	var tutorial = preload('res://scenes/main.tscn')
+	get_tree().change_scene_to(tutorial)
