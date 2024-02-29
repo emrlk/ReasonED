adb4b3a8153a72a0c0edbc6320f8e00bf4dd5d1d
Emily Louk <emilyrl222@gmail.com>
2024-02-28 20:40:41 -0500

    Add Idle Animation|Direction changes more accurate

--- StrawManny/scripts/player.gd
+++ StrawManny/scripts/player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
-onready var animation = $AnimationPlayer
+onready var animation = $AnimatedSprite
+
+
 
 # Read user input 60 times/second
 func _physics_process(delta):
@@ -7,6 +9,7 @@ func _physics_process(delta):
 	
 func _ready():
 	Engine.set_target_fps(Engine.get_iterations_per_second())
+
 	
 func read_input():
 	var velocity = Vector2()
@@ -14,35 +17,32 @@ func read_input():
 	var left = Input.is_action_pressed("left")
 	var up = Input.is_action_pressed("up")
 	var down = Input.is_action_pressed("down")
-
+	var lastXDirection
 	
-# Move the player and change its direction according to the keybind
+	if up or down or right or left:
+		# Play "run" animation when any movement key is pressed
+		animation.play("run")
+	else:
+		# Play "idle" animation when no movement keys are pressed
+		animation.play("idle")
+
+	#separate conditionals for actual motion and direction:
 	if up:
-		animation.play("run")
 		velocity.y -= 1
-
 	elif down:
-		animation.play("run")
 		velocity.y += 1
-	
-	
-	if right:
-		$Sprite2.flip_h = true
-		animation.play("run")
-		velocity.x += 1
 
-		
+	if right:
+		$AnimatedSprite.flip_h = true
+		velocity.x += 1
 	elif left:
-		$Sprite2.flip_h = false
-		animation.play("run")
+		$AnimatedSprite.flip_h = false
 		velocity.x -= 1
 
-	else:
-		#change to .play("idle") once ready
-		animation.stop()
 
-	# Prevent diagonal movement from faster than up, down, etc. individually
+	#Normalize vector to prevent diagonal movement from being faster
 	velocity = velocity.normalized()
+	#Speed of sprite:
 	velocity = move_and_slide(velocity * 600)
 	
 	
