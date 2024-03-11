d6338aa0766a5988f42d6d60589325a8d2835896
Emily Louk <emilyrl222@gmail.com>
2024-03-11 17:58:42 -0400

    edit homepage header styles

--- client/reasoned-app/src/app/page.tsx
+++ client/reasoned-app/src/app/page.tsx
@@ -22,8 +22,8 @@ const Page = () => {
 
       {/**Page Body */}
       <div className={"constainer bg-orange h-screen mx-auto flex justify-center items-center flex-col text-center"}>
-        <h1 className="font-bold text-8xl text-purple mb-3 hover:scale-105 transition duration-300">ReasonED<span className="text-white">.io</span></h1>
-        <p className="text-lg text-purple mb-16 hover:scale-105 transition duration-300">Empower Critical Thinking with Engaging Games!</p>
+        <h1 className="font-extrabold text-8xl text-purple mb-3 hover:scale-105 transition duration-300">ReasonED</h1>
+        <p className="text-2xl text-white mb-16 hover:scale-105 transition duration-300 font-bold">Empower Critical Thinking with Engaging Games</p>
         <div className="font-bold text-white text-xl flex flex-col gap-1/2">
           {/*<Button title="Elementary School" disabled={false} />*/}
           <div className="w-60 mb-5">
--- client/reasoned-app/src/app/teachers/page.tsx
+++ client/reasoned-app/src/app/teachers/page.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import Header from '@/components/common/header';
 import Footer from '@/components/common/footer';
+import Header from '@/components/common/header';
 
 export default function PrototypePage() {
     return (
@@ -10,12 +9,12 @@ export default function PrototypePage() {
 
             {/* Page Body */}
             <div className="constainer bg-orange min-h-screen mx-auto py-8">
-                <h2 className="text-4xl text-white font-bold mb-6">Teachers</h2>
+                <h2 className="text-4xl text-white font-bold mb-6 mx-30">Teachers</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-left">
 
                     {/* Left column */}
-                    <div className="flex flex-col justify-center">
-                        <p className="text-white mb-4">
+                    <div className="flex flex-col justify-center ">
+                        <p className="text-white mb-4 ">
                         ReasonED aims to support teachers in educating their students about logical fallacies. Our age-curated games introduce students to logical fallacies in a fun, interactive way. Each of our characters each embody a logical fallacy and need help learning to reason more accurately about the world around them. As students aid each character, they build the skills they need to combat these fallacies themselves. Be sure to visit our resources page for ways to incorporate ReasonED more fully in your classroom!
                         </p>
                     </div>
