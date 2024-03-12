b755696929a75bed06a9c4f98ff0d4760f679f11
Emily Louk <emilyrl222@gmail.com>
2024-03-11 22:44:17 -0400

    Add contact form, contact page

--- client/reasoned-app/src/app/resources/page.tsx
+++ client/reasoned-app/src/app/resources/page.tsx
@@ -12,12 +12,12 @@ export default function AboutReasonED() {
         <div className=" mb-5 mx-60 px-10 py-10">
           <h1 className="text-6xl text-white font-bold mb-10 ">Educational Resources</h1>
 
-          {/* Left column */}
+
           <div className="flex justify-center ">
             <p className="text-white mb-4 text-xl font-semibold">
             Use these additional resources to reinforce logical fallacy education in your classroom!
             Have resources of your own to offer? 
-            <Link href="teachers" className="link text-purple font-bold"> Contact Us!</Link>
+            <Link href="contact-us" className="link text-purple font-bold"> Contact Us!</Link>
             </p>
           </div>
         </div>
