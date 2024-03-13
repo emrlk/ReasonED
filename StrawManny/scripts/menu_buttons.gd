7766687098f5b9a8fe84a67f91dcb0a12f8d51b0
Dakota Dawe <ddawe001@odu.edu>
2024-03-13 11:46:17 -0400

    refactored player.gd

--- StrawManny/scripts/menu_buttons.gd
+++ StrawManny/scripts/menu_buttons.gd
@@ -10,4 +10,4 @@ func _on_Volume_pressed():
 
 func _on_Tutorial_pressed():
 	var tutorial = preload('res://scenes/main.tscn')
-	get_tree().change_scene_to(tutorial)
+	var _result = get_tree().change_scene_to(tutorial)
--- StrawManny/scripts/player.gd
+++ StrawManny/scripts/player.gd
@@ -1,50 +1,73 @@
 extends KinematicBody2D
+
 onready var animation = $AnimatedSprite
 
+export var move_right_action = String("right")
+export var move_left_action = String("left")
+export var move_up_action = String("up")
+export var move_down_action = String("down")
+
+export var run_animation = String("run")
+export var idle_animation = String("idle")
+
+var inputs = {
+	move_right_action: false,
+	move_left_action: false,
+	move_up_action: false,
+	move_down_action: false,
+}
 
 
 # Read user input 60 times/second
-func _physics_process(delta):
+func _physics_process(_delta):
 	read_input()
+	play_locomotion_animation(run_animation if is_any_input_down() else idle_animation)		
+	var velocity = get_velocity_from_input()
+	apply_velocity(velocity)
+	
 	
 func _ready():
 	Engine.set_target_fps(Engine.get_iterations_per_second())
 
-	
-func read_input():
-	var velocity = Vector2()
-	var right = Input.is_action_pressed("right")
-	var left = Input.is_action_pressed("left")
-	var up = Input.is_action_pressed("up")
-	var down = Input.is_action_pressed("down")
-	var lastXDirection
-	
-	if up or down or right or left:
-		# Play "run" animation when any movement key is pressed
-		animation.play("run")
-	else:
-		# Play "idle" animation when no movement keys are pressed
-		animation.play("idle")
 
+func is_any_input_down() -> bool:
+	return inputs.get(move_right_action) or inputs.get(move_left_action) or inputs.get(move_up_action) or inputs.get(move_down_action)
+
+
+# applies the given velocity to the character.
+# param velocity should not be normalized before being passed in, this function performs that operation
+func apply_velocity(velocity : Vector2):
+	if velocity.length() > 0:
+		#Normalize vector to prevent diagonal movement from being faster
+		velocity = velocity.normalized()
+		#Speed of sprite:
+		velocity = move_and_slide(velocity * 800)
+
+
+func play_locomotion_animation(animation_to_play : String):
+	animation.play(animation_to_play)
+
+
+func get_velocity_from_input() -> Vector2:
+	var velocity = Vector2()
 	#separate conditionals for actual motion and direction:
-	if up:
+	if inputs.get(move_up_action):
 		velocity.y -= 1
-	elif down:
+	elif inputs.get(move_down_action):
 		velocity.y += 1
 
-	if right:
+	if inputs.get(move_right_action):
 		$AnimatedSprite.flip_h = true
 		velocity.x += 1
-	elif left:
+	elif inputs.get(move_left_action):
 		$AnimatedSprite.flip_h = false
 		velocity.x -= 1
+	
+	return velocity
 
 
-	#Normalize vector to prevent diagonal movement from being faster
-	velocity = velocity.normalized()
-	#Speed of sprite:
-	velocity = move_and_slide(velocity * 800)
-	
-	
-
-	
+func read_input():
+	inputs[move_right_action] = Input.is_action_pressed(move_right_action)
+	inputs[move_left_action] = Input.is_action_pressed(move_left_action)
+	inputs[move_up_action] = Input.is_action_pressed(move_up_action)
+	inputs[move_down_action] = Input.is_action_pressed(move_down_action)
