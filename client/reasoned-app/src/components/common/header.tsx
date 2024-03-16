91c65a3eb11ac4027db0afa4aa6d3d0f38ad3202
Emily Louk <emilyrl222@gmail.com>
2024-03-15 21:38:05 -0400

    Center Navigation Links on header

--- client/reasoned-app/src/components/common/header.tsx
+++ client/reasoned-app/src/components/common/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className={"constainer bg-purple-shades-900 text-white  mx-auto flex items-center  px-6 py-2 h-24"}>
             <Link href="/">
                 {/*<div className="hidden md:block cursor-pointer">*/}
-                <div className="hidden md:block cursor-pointer mr-10 transform hover:scale-105 pl-20">
+                <div className="hidden md:block cursor-pointer ml-5 transform hover:scale-105 pl-20">
                     <Image
                         src="/ReasonEDLogo.png"
                         width={250}
@@ -20,7 +20,7 @@ export default function Header() {
             {/*<div className="grow">
                 <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white"></div>*/}
             <div className="flex-grow ">
-                <div className="flex items-center justify-start gap-2 md:gap-8 font-bold hover:white text-2xl ">
+                <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white text-2xl ">
 
                     {/* Students */}
                     <div className="relative transform hover:scale-110 transition-transform duration-100 ease-in-out">
