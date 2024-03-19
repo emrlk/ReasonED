4e9ecf04d0403efbde4d8b3f04f22191ca76ffbf
amorg025 <amorg025@odu.edu>
2024-03-18 23:46:40 -0400

    Adjust logged-in-students navbar to match spacing of regular navbar

--- client/reasoned-app/src/components/common/logged-in-student.tsx
+++ client/reasoned-app/src/components/common/logged-in-student.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 
 export default function LoggedInStudent() {
     return (
-        <div className={"constainer bg-purple text-white  mx-auto flex items-center  px-6 py-2 h-24"}>
+        <div className={"constainer bg-purple-shades-900 text-white  mx-auto flex items-center  px-6 py-2 h-24"}>
             <Link href="/student-home-page">
                 {/*<div className="hidden md:block cursor-pointer">*/}
-                <div className="hidden md:block cursor-pointer mr-5 transform hover:scale-105">
+                <div className="hidden md:block cursor-pointer ml-5 transform hover:scale-105 pl-20">
                     <Image
                         src="/ReasonEDLogo.png"
                         width={250}
@@ -21,7 +21,7 @@ export default function LoggedInStudent() {
             {/*<div className="grow">
                 <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white"></div>*/}
             <div className="flex-grow ">
-                <div className="flex items-center justify-start gap-2 md:gap-8 font-bold hover:white text-2xl ">
+                <div className="flex items-center justify-center gap-2 md:gap-8 font-bold hover:white text-2xl ">
 
                     {/* Profile */}
                     <div className="relative transform hover:scale-110 transition-transform duration-100 ease-in-out">
@@ -30,7 +30,7 @@ export default function LoggedInStudent() {
 
                     {/* Settings (Dropdown) */}
                     <div className="relative transform hover:scale-110 transition-transform duration-100 ease-in-out">
-                    <Link href="student-settings" className="link"><span className="link cursor-pointer transform hover:scale-105">Settings</span></Link>
+                        <Link href="student-settings" className="link"><span className="link cursor-pointer transform hover:scale-105">Settings</span></Link>
                         <div className="absolute z-10 mt-2 bg-white rounded shadow-md opacity-0 invisible transition-opacity duration-300">
                             <Link href="/edit-student-account" className="link block px-4 py-2 text-purple hover:bg-gray-200 rounded">Edit Account</Link>
                         </div>
@@ -40,11 +40,12 @@ export default function LoggedInStudent() {
                 </div>
             </div>
 
-            <div className="text-m flex items-center mr-10">
+            <div className="text-m flex items-center mr-10 pr-20">
                 {/* Sign Out */}
-                <div className="link text-white transform hover:scale-110 transition-transform duration-100 ease-in-out ">
-                    <Link href="/" className="font-bold">Sign Out</Link>
+                <div className="bg-orange text-white rounded-md px-3 py-1 mr-3 button hover:bg-orange-shades-300">
+                    <Link href="/" className="font-semibold">Sign Out</Link>
                 </div>
+
             </div>
         </div>
     );
