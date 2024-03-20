b00d5467bb730471ef8390319fe423c88de0385b
Emily Louk <emilyrl222@gmail.com>
2024-03-19 20:52:45 -0400

    Add Assets, Create main menu

---
+++ HastyHarry/Scenes/main_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_StartBtn_pressed():
+	get_tree().change_scene("res://Scenes/Player/customization/character_customization_level.tscn")
+
+
+func _on_TutorialBtn_pressed():
+	get_tree().change_scene("res://Scenes/Player/customization/character_customization_level.tscn")
