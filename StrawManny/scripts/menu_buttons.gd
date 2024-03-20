d9361099112902d7a570f127e3b2e1ff6cd4f7f9
June Troyer <jtroy003@odu.edu>
2024-03-19 22:31:03 -0400

    Basic HUD - level, score, lives, strength, mute

--- StrawManny/scripts/menu_buttons.gd
+++ StrawManny/scripts/menu_buttons.gd
@@ -1,13 +1,6 @@
 extends TextureButton
 
-var main_bus = AudioServer.get_bus_index('Master')
-
-
-func _on_Volume_pressed():
-	AudioServer.set_bus_mute(main_bus, not AudioServer.is_bus_mute(main_bus))
-
-
-
 func _on_Tutorial_pressed():
 	var tutorial = preload('res://scenes/main.tscn')
 	var _result = get_tree().change_scene_to(tutorial)
+
