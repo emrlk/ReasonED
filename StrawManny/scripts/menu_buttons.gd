f0d7ca7eb364973e5bd7b197a54c04947a0be9b6
Emily Louk <emilyrl222@gmail.com>
2024-04-02 17:12:14 -0400

    Change Resolution to 1920x1080, Adjust scenes

--- StrawManny/scripts/menu_buttons.gd
+++ StrawManny/scripts/menu_buttons.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
 func _on_Tutorial_pressed():
-	var tutorial = preload('res://scenes/main.tscn')
+	var tutorial = preload('res://scenes/Level1.tscn')
 	var _result = get_tree().change_scene_to(tutorial)
 
