c65f13d70dc24e0b81f4f00b6dc300be6f8371db
Emily Louk <emilyrl222@gmail.com>
2024-04-02 17:33:44 -0400

    Fix z-order of hud, knights

--- StrawManny/scenes/GameManager.gd
+++ StrawManny/scenes/GameManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var knight = preload("res://scenes/Knight.tscn")
 const NUM_KNIGHTS = 200
+const KNIGHT_LAYER = 1  # Adjust as needed to place the knights on the correct layer
 
 func _ready():
 	
@@ -26,5 +27,6 @@ func spawnKnights():
 	while i < NUM_KNIGHTS:
 		var randomKnight = knight.instance()
 		randomKnight.position = Vector2(rand_range(-40000,40000), rand_range(-40000,40000))
+		randomKnight.set_z_index(KNIGHT_LAYER)  # Set the Z-index to place the knight on the desired layer
 		add_child(randomKnight)
 		i += 1
