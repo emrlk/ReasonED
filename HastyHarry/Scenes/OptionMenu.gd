78baab8557a2830a458387ab4fe5b05d85019764
amorg025 <amorg025@odu.edu>
2024-04-15 17:46:42 -0400

    Create Option Menu scene and script

---
+++ HastyHarry/Scenes/OptionMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide() # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		# Toggle menu visibility when Escape key is pressed
+		if is_visible():
+			hide()
+		else:
+			show()
