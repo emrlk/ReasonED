890b15b0a06c139ad313539abd4c24e79b8b8780
Emily Louk <emilyrl222@gmail.com>
2024-04-15 23:56:58 -0400

    Add Strawman Scene w/ Animations

---
+++ StrawManny/scenes/MannyStrawman.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+export var idle_animation = String("idle")
+export var strike_animation = String("Strike")
+onready var animation = $AnimatedSprite
+onready var inventory = $Inventory
+onready var timer = $Timer
+onready var textPanel = $strawmanDefeatPanel
+var hasIdled = false
+
+func play_locomotion_animation(animation_to_play : String):
+	animation.play(animation_to_play)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	textPanel.hide()
+	animation.play("Strike")
+
+
+func _on_Timer_timeout():
+		textPanel.show()
+
+
+func _on_AnimatedSprite_animation_finished():
+	if hasIdled == false:
+		animation.play("idle")
+		hasIdled = true
